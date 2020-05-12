4e0399f5703933c12e718ed8c2ef36b8e2f1f1fa
lamecode <kislaya.sliva@gmail.com>
2020-05-12 17:27:22 +0300

    lab7 start

--- Lab7/lab7.rkt
+++ Lab7/lab7.rkt
@@ -1,9 +1,17 @@
  #lang racket
 (require srfi/13)
-(require "get-condition.rkt")
+(require "helper_functions.rkt" "get-condition.rkt")
 
 ;--------------------------------------------------------case-----------------------------------------------------------------------
-(define (having port command))
+(define (case port command)
+   (define before-case (string-split (substring command 0 (string-contains command "CASE")) " "))
+   (define after-end (string-split (substring command (string-contains command "END")) " "))
+   (define conditions (remove-duplicates (string-split (string-join (without-spaces (string-split (substring command
+                                      (+ (string-contains command "CASE") 4) (string-contains command " END")) "\"")) "") " WHEN ")))
+  (writeln before-case)
+  (writeln after-end)
+  conditions
+  )
 ;--------------------------------------------------------case-----------------------------------------------------------------------
 
 ;--------------------------------------------------------initialazer-----------------------------------------------------------------------
@@ -14,16 +22,16 @@
   (cond
     [(and (string-ci=? (substring option 0 6) "SELECT") (string-contains? option "CASE"))
     (case (open-input-string (file->string (list-ref (string-split option " ")
-                                             (- (length (string-split option " ")) 1)))) option)]
-    [(and (string-ci=? (substring option 0 6) "SELECT") (string-contains? option "GROUP BY") (string-contains? option "HAVING"))
-     (display (having (open-input-string (file->string (list-ref (string-split option " ")
-                                             (- (length (string-split option " ")) 1))))
-                                                                 (string-split option)))]
-    [(and (string-ci=? (substring option 0 6) "SELECT") (string-contains? option "GROUP BY"))
-     (display (group-by (open-input-string (file->string (list-ref (string-split option " ")
-                                           (- (length (string-split option " ")) 1))))
-                                           (string-split option)))]
-    [else (writeln "Невірно введено команду. Будь ласка, спробуйте ще.")])
+                                             (- (length (string-split option " ")) 1)))) option)])
+;    [(and (string-ci=? (substring option 0 6) "SELECT") (string-contains? option "GROUP BY") (string-contains? option "HAVING"))
+;     (display (having (open-input-string (file->string (list-ref (string-split option " ")
+;                                             (- (length (string-split option " ")) 1))))
+;                                                                 (string-split option)))]
+;    [(and (string-ci=? (substring option 0 6) "SELECT") (string-contains? option "GROUP BY"))
+;     (display (group-by (open-input-string (file->string (list-ref (string-split option " ")
+;                                           (- (length (string-split option " ")) 1))))
+;                                           (string-split option)))]
+;    [else (writeln "Невірно введено команду. Будь ласка, спробуйте ще.")])
  )
 
 (cli )
